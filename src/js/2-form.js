453666828e230156e045e50902db41d3413dbab3
Igor <igorivandikov87@gmail.com>
2024-06-05 20:37:39 +0300

    form done

---
+++ src/js/2-form.js
@@ -0,0 +1,58 @@
+
+// step1=============
+
+let formData = {
+    email: "",
+    message: "",
+}
+
+
+
+// step2=============
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', (event) => {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+
+window.addEventListener('load', () => {
+    const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (savedFormData) {
+        formData = savedFormData;
+        for (let field in formData) {
+            form.elements[field].value = formData[field];
+        }
+    }
+});
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    for (let field in formData) {
+        if (!formData[field]) {
+            alert('Будь ласка, заповніть усі поля');
+            return;
+        }
+
+    }
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    formData = {email:"", message: ""};
+    form.reset();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
